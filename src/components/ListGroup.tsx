c2e27b83a1a63d15d48f7b6fefe3c474c342c266
Marcio Fao <marcio.lopes.fao@gmail.com>
2026-02-04 09:14:23 -0300

    👔 click handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,35 @@
+import { Fragment } from "react/jsx-runtime";
+import { MouseEvent } from "react";
+
 function ListGroup() {
+    let items = ["item1", "item2", "item3", "item4", "item5"];
+    //items=[];
+
+    let selcetedItem = -1;
+
+    const getMessage = () => {
+        return items.length === 0 && <p>There are no items</p>;
+    };
+
+    const handleClick = (event: MouseEvent) => console.log(event);
+
     return (
-        <ul className="list-group">
-            <li className="list-group-item">item</li>
-            <li className="list-group-item">item</li>
-            <li className="list-group-item">item</li>
-            <li className="list-group-item">item</li>
-            <li className="list-group-item">item</li>
-        </ul>
+        <>
+            <h1>List</h1>
+            {getMessage()}
+            <ul className="list-group">
+                {items.map((item, index) => (
+                    <li
+                        key={item}
+                        onClick={handleClick}
+                        className={selcetedItem === index ? 'list-group-item active' : 'list-group-item'}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
 
 export default ListGroup;
-    
