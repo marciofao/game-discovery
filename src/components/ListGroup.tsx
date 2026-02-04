530ddec3f93512e1e61a995c18db3766d68d6e81
Marcio Fao <marcio.lopes.fao@gmail.com>
2026-02-04 08:42:33 -0300

    ➕ added bootstrap

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,14 @@
+function ListGroup() {
+    return (
+        <ul className="list-group">
+            <li className="list-group-item">item</li>
+            <li className="list-group-item">item</li>
+            <li className="list-group-item">item</li>
+            <li className="list-group-item">item</li>
+            <li className="list-group-item">item</li>
+        </ul>
+    );
+}
+
+export default ListGroup;
+    
