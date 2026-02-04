3f572f2d30cd24a6b9579e2e78c071323bce8f65
Marcio Fao <marcio.lopes.fao@gmail.com>
2026-02-04 09:19:28 -0300

    selecting list items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,15 @@
+import { useState } from "react";
 import { Fragment } from "react/jsx-runtime";
-import { MouseEvent } from "react";
 
 function ListGroup() {
     let items = ["item1", "item2", "item3", "item4", "item5"];
-    //items=[];
 
-    let selcetedItem = -1;
+    const [selectedItem, setSelectedItem] = useState(-1);
+    
 
     const getMessage = () => {
         return items.length === 0 && <p>There are no items</p>;
     };
-
-    const handleClick = (event: MouseEvent) => console.log(event);
 
     return (
         <>
@@ -21,8 +19,8 @@ function ListGroup() {
                 {items.map((item, index) => (
                     <li
                         key={item}
-                        onClick={handleClick}
-                        className={selcetedItem === index ? 'list-group-item active' : 'list-group-item'}
+                        onClick={()=>setSelectedItem(index)}
+                        className={selectedItem === index ? 'list-group-item active' : 'list-group-item'}
                     >
                         {item}
                     </li>
