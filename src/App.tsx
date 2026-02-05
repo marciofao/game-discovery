fdb11da66e88ff22bae08f6f91c8a8c1c40a8b3a
Marcio Fao <marcio.lopes.fao@gmail.com>
2026-02-05 17:09:55 -0300

    passing props to button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import ListGroup from './components/ListGroup';
 import Alert from './components/Alert';
+import Button from './components/Button';
 
 function App(){
 
@@ -14,8 +15,8 @@ function App(){
             Hello <span>World</span>
         </Alert>
         <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem}/>
-        
+        <Button color='secondary' onClick={()=>console.log('clicked')}>My button</Button>
     </div>
-}
+} 
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import React, { Children } from 'react'
+
+interface Props{
+    children: string;
+    color: string;
+    onClick: () => void;
+}
+
+const Button = ({ children, color, onClick }: Props) => {
+    return (
+        <button className={"btn btn-"+color} onClick={onClick}>
+            {children}
+        </button>
+    );
+}
+
+export default Button
